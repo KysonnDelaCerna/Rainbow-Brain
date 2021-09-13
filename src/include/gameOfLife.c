14e88695d6ebb2426ca9f140bbe110fc781705c2
unknown <kysonn_delacerna@dlsu.edu.ph>
2021-09-13 15:37:26 +0800

    working for both

--- src/include/gameOfLife.c
+++ src/include/gameOfLife.c
@@ -3,50 +3,6 @@
 #include <stdio.h>
 #include "gameOfLife.h"
 
-int mod(int a, int b)
-{
-    return (a + b) % b;
-}
-
-char nextState(char slice[9])
-{
-    if (slice[4] == RED_DYING || slice[4] == GREEN_DYING || slice[4] == BLUE_DYING)
-        return DEAD;
-    if (slice[4] == RED_ALIVE)
-        return RED_DYING;
-    if (slice[4] == GREEN_ALIVE)
-        return GREEN_DYING;
-    if (slice[4] == BLUE_ALIVE)
-        return BLUE_DYING;
-    
-    int i, red, green, blue;
-    red = green = blue = 0;
-
-    for (i = 0; i < 9; i++)
-    {
-        if (i == 4)
-            continue;
-        else if (slice[i] == RED_ALIVE)
-            red++;
-        else if (slice[i] == GREEN_ALIVE)
-            green++;
-        else if (slice[i] == BLUE_ALIVE)
-            blue++;
-    }
-
-    if (red + green + blue == 4)
-    {
-        if (red >= 2)
-            return RED_ALIVE;
-        if (green >= 2)
-            return GREEN_ALIVE;
-        if (blue >= 2)
-            return BLUE_ALIVE;
-    }
-
-    return DEAD;
-}
-
 void initiateBoard(char board[HEIGHT][WIDTH])
 {
     int i, j;
--- src/include/gameOfLife.h
+++ src/include/gameOfLife.h
@@ -1,8 +1,8 @@
 #ifndef __GAME_OF_LIFE_H
 #define __GAME_OF_LIFE_H
 
-#define WIDTH 10
-#define HEIGHT 10
+#define WIDTH 32
+#define HEIGHT 32
 
 #define RED_ALIVE 6
 #define RED_DYING 5
@@ -12,8 +12,6 @@
 #define BLUE_DYING 1
 #define DEAD 0
 
-int mod(int, int);
-char nextState(char[9]);
 void initiateBoard(char[HEIGHT][WIDTH]);
 void displayBoard(char[HEIGHT][WIDTH]);
 void seedRandom();
